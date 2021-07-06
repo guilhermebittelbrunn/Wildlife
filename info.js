94c4545867179af62a89e438346c664dc1066b3f
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2021-07-06 20:40:43 -0300

    Add info
    -Raposa info
    -Koala info
    -Tucano info
    -css changes
    -Script js att

--- info.js
+++ info.js
@@ -2,10 +2,43 @@ var raposa = {
     'id': 'raposa',
     'nome': 'Raposa-vermelha',
     'peso': 10,
-    'idade': 12,
+    'idade': 5,
+    'texto2': 'As raposas vermelhas (Vulpes vulpes) são mamíferos, carnívoros, pertencente à família Canidae. Este animal é uma das várias espécies do gênero Vulpes, gênero ao qual pertencem a maioria das espécies de raposa, embora algumas espécies pertençam a outros gêneros.',
+    'texto3': 'A raposa vermelha, como evidencia o nome, tem a pelagem vermelho-amarelada. Chega a medir 1m de comprimento, fora seu rabo que é espesso e longo (mede de 32 a 49cm), que geralmente tem a ponta branca. Alcança 40cm de altura, e pode pesar até 10 kg. Suas orelhas são grandes, mais escuras que o tom de sua pelagem por fora, e brancas por dentro. Os sentidos das raposas são muito apurados.',
+    'texto4': 'As raposas vermelhas são animais de hábitos crepusculares e noturnos. Alimentam-se preferencialmente de pequenos roedores, ovos e filhotes de aves, coelhos e eventualmente torna-se necrófaga, ou seja, alimenta-se de carne em decomposição. Frutas e grãos compõem sua alimentação no outono.',
+    'texto5': 'Algumas pessoas são como raposas pois são imprecisas e inconsistentes, outras são como porcos-espinhos pois focam o essencial, ignoram o resto e são eficazes.“ —  James C. Collins 1958',
+    'texto6': 'Esses animais são encontrados em toda a Europa (inclusive na Grã-Bretanha), Ásia, Oriente Médio, norte da África, Austrália e na América do Norte. Vive, portanto, em hábitats diversos, geralmente em tocas, evitando apenas os desertos e as florestas muito densas.',
+    'texto7': 'Estima-se que existam aproximadamente 48 subespécies de raposas vermelhas, porém essa informação não foi comprovada cientificamente. As raposas vermelhas vivem até 12 anos em seu habitat natural, sobretudo em regiões em que a caça não é permitida. Em cativeiro chegam a viver até 15 anos.',
+    'caracteristicas': ['Surgiu: 200 anos', 'Tipo: Mamífero', 'Idade Média: 4 anos', 'Macho adulto: 4.2-9 kg', 'Fêmea adulta: 3.7-8kg', 'Família: Canídeos']
 }
 var koala = {
-    'nome': 'Koala',
-    'peso': 20,
-    'idade': 18,
+    'id': 'Koala',
+    'nome': 'Coala',
+    'peso': 13,
+    'idade': 15,
+    'texto2': 'Os coalas são animais australianos, pertencentes ao grupo dos mamíferos marsupiais, isto é, possuem uma bolsa ventral, chamada de bolsa marsupial ou marsúpio, para o desenvolvimento dos filhotes que nascem prematuros, assim como acontece com os cangurus da mesma região e os gambás que podem ser vistos no Brasil. Na Austrália, os coalas podem ser encontrados na região de florestas a nordeste e sudeste do país.',
+    'texto3': 'Os coalas são animais lentos que raramente descem das árvores e bebem pouca água, recorrendo ao líquido apenas em caso de doenças ou extrema desidratação devido ao calor. Geralmente, conseguem todo o líquido que necessitam através da alimentação que consiste basicamente em folhas de algumas espécies de eucalipto. As folhas são cuidadosamente escolhidas, e frequentemente as mais novas são recolhidas. Após ingeri-las as folhas permanecem em suas bochechas para serem mastigadas aos poucos e a absorção do nutriente acontece em seu longo intestino.',
+    'texto4': 'As principais características desse animal são: a cabeça grande, o focinho curto e os olhos bem separados. O nariz achatado com as narinas em forma de V tem papel importante no equilíbrio térmico do corpo. O pelo do animal é denso e macio desempenhando importante função na proteção contra os fatores meteorológicos e regulação térmica. Como eles não constroem abrigos, ficam constantemente expostos a chuvas e o Sol. A pelagem no dorso dos coalas é bastante densa e ficam mais escurecidas durante o inverno para captar mais o calor do Sol e durante o verão tornam-se mais escassas para não aquecer o corpo do animal.',
+    'texto5': 'Sou como um coala. Gosto de estar desacompanhada. Gosto de ser solitária. Gosto de ser eu',
+    'texto6': 'Os coalas possuem poucos predadores, prevalecendo o homem que os caçam pela pelagem e os aborígenes para se alimentar preferindo o coala por serem lentos e quando ameaçados não fugirem. No ambiente natural o cachorro selvagem (Canis dingo) se alimenta dos coalas velhos ou doentes, já que os jovens e de boa saúde podem machucar o cachorro com facilidade com suas garras.',
+    'texto7': 'A principal ameaça sofrida pelos coalas são a caça desses animais para retirar sua pele e vendê-la a indústrias têxteis que fabricam casacos com esses animais. Além disso, o desmatamento que diminui a área alimentícia e as queimadas que além de destruir as árvores ainda matam inúmeros animais que não conseguem fugir a tempo devido ao seu deslocamento ser lento.',
+    'caracteristicas': ['Surgiu: 230 anos', 'Tipo: Mamífero', 'Idade Média: 15 anos', 'Macho adulto: 4-9 kg', 'Fêmea adulta: 4-7kg', 'Família: Phascolarctidae']
 }
+var tucano = {
+    'id': 'tucano',
+    'nome': 'Tucano',
+    'peso': 0.6,
+    'idade': 40,
+    'texto2': 'Os tucanos são aquelas espécies de aves pertencentes à Ordem Piciforme, Família Ramphastida, composta por cerca de 30 espécies encontradas principalmente no Brasil, México e Argentina. Essa ordem aviária é caracterizada por apresentar pés zigomáticos (nos quais a 2ª e 3ª falange são voltadas para frente e a 1ª e 4ª falange são voltadas para trás) e glândula uropigial (secreta o óleo utilizado na impermeabilização das penas) bipartida. Além dessas características, os ranfastídeos apresentam ainda ausência de inglúvio (papo) e presença de um bico córneo pneumático que é bem leve e resistente. O bico participa também da dispersão de calor, por ser muito vascularizado. São aves sem hábitos migratórios, arborícolas, que vivem em bando e se alimentam principalmente de frutas, complementando a dieta com alguns insetos e pequenas presas.',
+    'texto3': 'Os tucanos podem chegar a 60 centímetros de comprimento. Eles possuem asas curtas e arredondadas, bem como uma longa cauda, porém sua característica principal é o enorme bico, que em algumas espécies pode medir até 22 centímetros. O tucano consegue voar porque, apesar do tamanho, esse bico não é pesado, pois é feito de osso leve coberto de queratina (o mesmo material de que são feitas as unhas humanas).',
+    'texto4': 'Em muitos tucanos, o bico tem tons de verde, vermelho, branco ou amarelo. Essas cores ajudam a distinguir uma espécie da outra. No Brasil, as espécies mais populares são o tucano-toco, de bico amarelo, e seus parentes tucano-de-bico-preto e tucano-de-bico-verde. O tucano-toco é o maior deles, chegando a pesar cerca de 500 gramas.',
+    'texto5': 'Tucano colorido como o o crepúsculo, tão lindo quanto',
+    'texto6': 'Os tucanos formam ninhos no alto do tronco de árvores, acomodando-se em buracos naturais ou em ninhos de pica-pau abandonados. Cada fêmea bota de dois a quatro ovos, que eclodem após cerca de dezoito dias. Os filhotes nascem de olhos fechados, sem plumagem nenhuma e com um bico largo, porém curto. Os pais cuidam da cria durante cerca de 45 dias, até que os filhotes estejam prontos para voar. Famílias de tucanos podem continuar juntas mesmo após esse período.',
+    'texto7': 'Os tucanos alimentam-se principalmente de frutas, mas também podem comer insetos, cobras, rãs e lagartos. Muitas vezes eles invadem os ninhos de pássaros canoros e comem seus ovos ou até mesmo seus filhotes. Para se alimentar, o tucano pega a comida com a ponta do bico e joga a cabeça para trás, mandando o alimento para a garganta. As bordas serrilhadas do bico ajudam a quebrar frutas grandes em pedaços menores.',
+    'caracteristicas': ['Surgiu: 280 anos', 'Tipo: Aves', 'Idade Média: 40 anos', 'Macho adulto: 600g', 'Fêmea adulta: 540g', 'Família: Ramphastidae']
+}
+
+
+
+
+                
--- script.js
+++ script.js
@@ -2,16 +2,25 @@ let Nome = document.querySelector('header h1')
 let kg = document.getElementsByClassName('card')[0]
 let age = document.getElementsByClassName('card')[1]
 let imgs = document.getElementsByTagName('img')
+let ps = document.getElementsByTagName('p')
+let ul = document.querySelector('.caracteristicas ul')
 
 function info(animal){
+    let lista = []
     Nome.innerText = animal.nome
     kg.children[0].innerText = animal.peso
     age.children[0].innerText = animal.idade
     for (let foto = 4; foto < imgs.length; foto++){
         imgs[foto].setAttribute('src', `images/jpg/${animal.id}${foto}.jpg`)
     }
+    for (let p = 2; p < ps.length; p++){
+        var texto = 'texto' + p
+        ps[p].innerHTML = animal[texto]
+    }
+    for (let item = 0; item < animal.caracteristicas.length; item++){
+        lista.push(`<li>${animal.caracteristicas[item]}</li>`)
+        ul.innerHTML = lista
+    }
 }
 
-
-
-let ps = document.getElementsByTagName('p')
+info(raposa)
