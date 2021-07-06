70b89aa2056365d86faef8e57d3dca521cb62665
Guilherme Bittelbrunn <guilherme.bitte@unifebe.edu.br>
2021-07-06 18:47:22 -0300

    Style
    -responsive
    -design toggle menu
    -some js codes
    -new images

--- info.js
+++ info.js
@@ -1,4 +1,5 @@
 var raposa = {
+    'id': 'raposa',
     'nome': 'Raposa-vermelha',
     'peso': 10,
     'idade': 12,
--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-Nome = document.querySelector('header h1')
-kg = document.getElementsByClassName('card')[0]
-age = document.getElementsByClassName('card')[1]
+let Nome = document.querySelector('header h1')
+let kg = document.getElementsByClassName('card')[0]
+let age = document.getElementsByClassName('card')[1]
+let imgs = document.getElementsByTagName('img')
 
 function info(animal){
     Nome.innerText = animal.nome
     kg.children[0].innerText = animal.peso
     age.children[0].innerText = animal.idade
+    for (let foto = 4; foto < imgs.length; foto++){
+        imgs[foto].setAttribute('src', `images/jpg/${animal.id}${foto}.jpg`)
+    }
 }
+
+
+
+let ps = document.getElementsByTagName('p')
